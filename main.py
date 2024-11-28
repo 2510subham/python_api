1c30c2fd74f17dc324ab4f616414904c28798ac4
2510subham <subhamagarwal400@gmail.com>
2024-11-28 12:51:31 +0530

    fixed

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
+
 import subprocess
 app = FastAPI()
 
 @app.get("/ping")
 def read_root():
-    return {"ping": "pong"}
+    try:
+        return {"ping": "pong"}
+    except Exception as e:
+        return {"error": str(e)}
 
-@app.get("/run-script")
-def run_script():
+@app.get("/run")
+def run_script(authorization: str = Header("")):
     try:
         script_path = "/C/Users/gangs/Music/python-api/hello.sh>py.log"  
         bash_path = "C:/Program Files/Git/bin/bash.exe"
+        # want to verify header also
+        # authorization: str = Header(...)
+        print("Script path: ", authorization)
+        if authorization != "abcd1234":
+            raise HTTPException(status_code=401, detail="Unauthorized")
         result = subprocess.run(
             [bash_path, "-c", script_path], 
             check=True,   
@@ -21,12 +30,9 @@ def run_script():
         )
         return {
             "status": "success",
-            "message": "Script executed successfully",
+            "detail": "Script executed successfully",
             # "stderr": result.stderr.decode("utf-8"),
         }
     except subprocess.CalledProcessError as e:
         print(e)
-        return {
-                "status": "error",
-                "message": "Script execution failed",
-            },
+        raise HTTPException(status_code=401,detail="Script Execution Failed")
